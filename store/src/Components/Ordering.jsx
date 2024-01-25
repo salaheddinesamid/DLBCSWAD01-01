4100874508f6cb9a2a70f9cd46311ec4174fdc7e
salaheddinesamid <salaheddine.kobra@iu-study.org>
2024-01-25 13:08:04 +0100

    back

--- store/src/Components/Ordering.jsx
+++ store/src/Components/Ordering.jsx
@@ -19,7 +19,6 @@ export function Ordering(props){
     return(
         <div class="row mt-4">
             <h3>Order Here:</h3>
-            <h1>Hello</h1>
             <div class="col-xl-7">
                 <div class="row ms-4 mt-4">
                     <h4>Products:</h4>
